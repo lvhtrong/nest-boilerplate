8ef540a9d2bb7b98cc9174d317dd3154f1fb428e
Trong Lu <trong.lu@contemi.com.vn>
2021-01-04 16:59:02 +0700

    configure mongodb and mongoose

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,4 +1,5 @@
 import { Global, HttpModule, Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigService } from './services/config.service';
 import { GeneratorService } from './services/generator.service';
 import { ValidatorService } from './services/validator.service';
@@ -8,7 +9,17 @@ const providers = [ConfigService, ValidatorService, GeneratorService];
 @Global()
 @Module({
   providers,
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        return {
+          uri: configService.get('DB_URI'),
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   exports: [...providers, HttpModule],
 })
 export class SharedModule {}
